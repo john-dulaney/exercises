db390021bb57298b95769ba188e53dc06f8620fb
John Dulaney <j@Johns-MacBook-Pro.local>
2017-10-27 14:52:22 -0500

    AHHHHHH FRIDAY

--- javascript/Lexscopistan/heap-skope-ops.js
+++ javascript/Lexscopistan/heap-skope-ops.js
@@ -2,41 +2,55 @@ console.log("Cells within cells within cells interlinked. INTERLINKED")
 const gemHeapSkope = function () { 
     // Resource contained inside
 
-const gemMine = {
-    "Onyx": {
-    "kilograms": 453
-        },
+    const gemMine = {
+        "Onyx": {
+        "kilograms": 453
+            },
 
-    "Amethyst": {
-    "kilograms": 453
-        },
+        "Amethyst": {
+        "kilograms": 453
+            },
 
-    "Bloodstone": {
-    "kilograms": 453
-        },
+        "Bloodstone": {
+        "kilograms": 453
+            },
 
-    "Emerald": {
-    "kilograms": 453
-        }
-//1,812 we should end with ~90kg per after processing functions run
-    }
+        "Emerald": {
+        "kilograms": 453
+            }
+                //1,812 we should end with ~90kg per after processing functions run
+   }
         return {
             "process": function (requestedMineral) {
-                let requestedMineral
-                if ( gemMine >= 5) {
-                    gemMine -= 5
-                    requestedMineral++
-                
-            }
-            //something goes here function??
-            
-            return {
-                "mineral": requestedMineral,
-                "amount": gemMine // Change this to the correct amount
+                for (let key in gemMine) {
+                    // if (gemMine.hasOwnProperty(key)) {
+                        const mineAmount = gemMine[key].kilograms;
+                        console.log(mineAmount)
+                        
+                    }
+                }
             }
         }
-    }   
-}
+//             //something goes here function??
+            
+//             return {
+//                 "mineral": requestedMineral,
+//                 "amount": gemMine.kilograms // Change this to the correct amount
+//             }
+//         }
+//     }   
+// }
+//     const storageGen = function* () {
+//         let currentContainer = 1
+//         const maximumContainers = 30
+        
+//         while (currentContainer <= maximumContainers) {
+//             yield { "id": currentContainer, "mineral": "amount", "": [] }
+//             currentContainer++
+            
+//         }
+//     }
+//     console.log(gemHeapSkope)
 /*
 The SkopeManager variable represents the object with the
 `process` method on it.
@@ -52,18 +66,7 @@ left in the gem mine.
 // Create a generator for 30 storage containers, which is how many a hëap-skope
 // is equipped with.
 
-const storageGen = function* () {
-    let currentContainer = 1
-    const maximumContainers = 30
-    
-    while (currentContainer <= maximumContainers) {
-        yield { "id": currentContainer, "mineral": "amount", "": [] }
-        currentContainer++
-        
-    }
-  }
-
-// const sumValues = gemMine => Object.values(gemMine).reduce((a, b) => a + b)
+const sumValues = gemMine => Object.values(gemMine).reduce((a, b) => a + b)
 // let gemMine = {
 //     value: 5,
 //     [Symbol.iterator]: function* () {

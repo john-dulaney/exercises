7d39847d709c92fb2bea0e58f66e2ae76c92431c
John Dulaney <j@Johns-MacBook-Pro.local>
2017-10-30 14:19:16 -0500

    commit before i screw anything up

---
+++ javascript/db.js
@@ -0,0 +1,25 @@
+// examples
+        // orderId
+        // EMployeeID
+        // Date
+        // Total
+        // PaymentID
+        // LocationID
+        // CustomerID
+
+
+
+
+
+// const locations =[ 
+//     {
+//     "locationId": 1,
+//     "location": "usa"
+//     }
+// ]
+
+// // customerID
+// Named
+// email
+// phone
+// memberID
